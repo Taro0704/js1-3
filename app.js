a3a8d95f40af8660ebb6610162758b584add27ed
Taro <nin788703@gmail.com>
2023-11-30 08:51:47 +0900

    課題と表記が同じになりました

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
-const name = prompt('タスクを入力してください');
-window.alert('新しいタスクを追加しました');
-window.prompt('「確認、追加、削除、終了」の４つのいずれかを入力してください')
-
 console.log("======================");
 
 console.log("現在持っているタスク一覧");
 
 console.log("======================");
 
-console.log("0:掃除\n1:買い物\n2:散歩")
+const tasks = ['掃除','買い物','散歩'];
+
+for (let i = 0; i < tasks.length; i++) {
+  console.log(i + `:` + tasks[i]);
+};
+
+const name = prompt('タスクを入力してください');
+window.alert('新しいタスクを追加しました');
+// window.prompt('「確認、追加、削除、終了」の４つのいずれかを入力してください')
 
 console.log("======================");
 
@@ -20,11 +24,10 @@ console.log("======================");
 const characters = ['掃除','買い物','散歩',name];
 
 for (let i = 0; i < characters.length; i++) {
-
   console.log(i + `:` + characters[i]);
 };
   
-
+window.prompt('「確認、追加、削除、終了」の４つのいずれかを入力してください')
 
 
 
